73d779f128f565839aeed6c3fdee1265e4618bb9
mdsa97087 <mdsa97087@gmail.com>
2022-09-30 08:58:08 +0530

    day2

--- react-unit-4-project/src/Components/Product.jsx
+++ react-unit-4-project/src/Components/Product.jsx
@@ -1,24 +1,65 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./style/Product.css";
 
 function Product() {
   return (
     <div>
-    
-    <Navbar/>
-    Product
-   aaaaaaaaaaaaaaaaaaaaaaaaaa
-   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-   ssssssssssssssssssssssssssssssssssssss
-   ggggggggggggggggggggggggggggggggggggggggggggggggggggggg
-   hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-   <img src="https://static.airtable.com/images/homescreen/homepage-apps-dashboard.jpg
-   " alt="" />
+      <Navbar />
 
-   <img src="https://static.airtable.com/images/homescreen/homepage-apps-dashboard.jpg
-   " alt="" />
+      <div className="productbigdiv">
+        <div className="Scrolldiv">Drop Down Scroll</div>
+
+        <div className="imgdivsame">
+          <div className="same1">
+            <div className="samefirst">
+              <div className="imgback">
+                <h3>Content Calender </h3>
+              </div>
+            </div>
+            <div className="samefirst1">
+            <div className="imgback1">
+              <h3>Project Tracker</h3>
+              </div>
+            </div>
+            <div className="samefirst2">
+            <div className="imgback2">
+              <h3>Marketing Tracking</h3>
+              </div>
+            </div>
+          </div>
+          <div className="same2">
+            <div className="samesecond">
+              <h4>Product Teams</h4>
+            </div>
+            <div className="samesecond">
+              <h4>Product Catalog</h4>
+            </div>
+            <div className="samesecond">
+              <h4>Product Launch</h4>
+            </div>
+            <div className="samesecond">
+              <h4>Bug Tracker</h4>
+            </div>
+          </div>
+          <div className="same2">
+            <div className="samesecond">
+              <h4>Event Planning</h4>
+            </div>
+            <div className="samesecond">
+              <h4>User Reserch</h4>
+            </div>
+            <div className="samesecond">
+              <h4>Simple Applicant</h4>
+            </div>
+            <div className="samesecond">
+              <h4>Digital Video</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
